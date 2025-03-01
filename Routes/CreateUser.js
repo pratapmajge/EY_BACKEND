f3f01112b3f233146a1e364e7d4ae6b41ca2ab08
pratapmajge <majgepratap1504@gmail.com>
2025-03-01 15:02:23 +0530

    3

--- Routes/CreateUser.js
+++ Routes/CreateUser.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator')
-
+const bcrypt = require('bcrypt')
+const jwt= require('jsonwebtoken')
+const jwtSecret="pratapmajgehfjfjfhfbdndggg"
 router.post("/createuser",
     [
         body('email').isEmail(),
@@ -14,11 +16,12 @@ router.post("/createuser",
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-
+        const salt = await bcrypt.genSalt(10)
+        let secPassword= await bcrypt.hash( req.body.password, salt)
         try {
             await User.create({
                 name: req.body.name,
-                password: req.body.password,
+                password: secPassword,
                 email: req.body.email,
                 location: req.body.location
             })
@@ -47,12 +50,19 @@ router.post("/loginuser",
             if (!userData) {
                 return res.status(400).json({ errors: "Try logging in with the correct email" })
             }
+            const pwdCompare=await bcrypt.compare(req.body.password, userData.password)
 
-            if (req.body.password !== userData.password) {
+            if (!pwdCompare) {
                 return res.status(400).json({ errors: "Incorrect password" })
             }
 
-            return res.json({ success: true })
+            const data ={
+                user:{
+                    id:userData.id
+                }
+            }
+            const authToken = jwt.sign(data,jwtSecret)
+            return res.json({ success: true , authToken:authToken })
 
         } catch (error) {
             console.log(error)
